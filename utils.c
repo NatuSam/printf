b072dbd1cdd5731e3d07f6e8c7df5f74834a8130
NatuSam <natnaelsamsonkebede@gmail.com>
2023-10-16 08:07:08 -0700

    C - printf

--- utils.c
+++ utils.c
@@ -2,8 +2,7 @@
 /**
 * is_printable - Evaluates if a char is printable
 * @c: Char to be evaluated.
-**
-Return: 1 if c is printable, 0 otherwise
+* Return: 1 if c is printable, 0 otherwise
 */
 int is_printable(char c)
 {
@@ -33,8 +32,7 @@ return (3);
 /**
 * is_digit - Verifies if a char is a digit
 * @c: Char to be evaluated
-**
-Return: 1 if c is a digit, 0 otherwise
+* Return: 1 if c is a digit, 0 otherwise
 */
 int is_digit(char c)
 {
@@ -46,8 +44,7 @@ return (0);
 * convert_size_number - Casts a number to the specified size
 * @num: Number to be casted.
 * @size: Number indicating the type to be casted.
-**
-Return: Casted value of num
+* Return: Casted value of num
 */
 long int convert_size_number(long int num, int size)
 {
@@ -61,8 +58,7 @@ return ((int)num);
 * convert_size_unsgnd - Casts a number to the specified size
 * @num: Number to be casted
 * @size: Number indicating the type to be casted
-**
-Return: Casted value of num
+* Return: Casted value of num
 */
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
