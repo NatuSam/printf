8907960ea9cc31cacbf1c2662675f4aaba1a42cd
Muluken <mulukeneshetu70@gmail.com>
2023-10-18 08:20:50 -0700

    C - printf

--- main.c
+++ main.c
@@ -2,29 +2,20 @@
 #include <stdio.h>
 #include "main.h"
 
-/*
+/**
  * main - Entry point
  *
  * Return: Always 0
  */
 int main(void)
 {
-int len, len2;
+int len;
+int len2;
 unsigned int ui;
 void *addr;
 
 len = _printf("%.6s", "Best School !\n");
 len2 = printf("%.6s", "Best School !\n");
-fflush(stdout);
-if (len != len2)
-{
-printf("Lengths differ.\n");
-fflush(stdout);
-return (1);
-}
-
-len = _printf("Let's try to printf a simple sentence.\n");
-len2 = printf("Let's try to printf a simple sentence.\n");
 ui = (unsigned int)INT_MAX + 1024;
 addr = (void *)0x7ffe637541f0;
 _printf("Length:[%d, %i]\n", len, len);
@@ -47,7 +38,7 @@ len = _printf("Percent:[%%]\n");
 len2 = printf("Percent:[%%]\n");
 _printf("Len:[%d]\n", len);
 printf("Len:[%d]\n", len2);
-_printf("Unknown:[%%r]\n");
-printf("Unknown:[%%r]\n");
+_printf("Unknown:[%r]\n");
+printf("Unknown:[%r]\n");
 return (0);
 }
